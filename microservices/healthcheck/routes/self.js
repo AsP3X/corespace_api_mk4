353320ebca4c1ff1d24400e0303855151d9d4d98
AsP3X <zombymedia@gmail.com>
2022-11-08 23:34:30 +0100

    TASK: change name of mircoservice health route

--- microservices/healthcheck/routes/self.js
+++ microservices/healthcheck/routes/self.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const Logger = require('../assets/utils/logger');
 
 // Create the logger
-const logger = new Logger("healthcheck/root");
+const logger = new Logger("healthcheck/self");
 
 // Importing router
 const router = express.Router();
