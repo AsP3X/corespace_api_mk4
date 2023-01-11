96c802c9fb37362d864fe92dc6df4aa5130442d8
AsP3X <services@vorberg-webmail.de>
2023-01-11 12:07:14 +0100

    TASK: added tokenSchema

---
+++ microservices/token/assets/models/token.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const tokenSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  name: { type: String, required: true },
+  token: { type: String, required: true },
+  description: { type: String, required: true },
+  status: { type: String, required: true },
+  createTimestamp: { type: Number, required: true },
+});
+
+const Token = mongoose.model('Token', tokenSchema);
+
+module.exports = Token;
